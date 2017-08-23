4810e6ce8454ca4862de2a27201b82454547bc93
Aracely Perez Yuca <31052232+AracelyZ@users.noreply.github.com>
2017-08-22 19:02:18 -0500

    Add files via upload

---
+++ Calculadora_6A/Calculadora_6A.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora_6A.Libreria;
+
+namespace Calculadora_6A.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos misMetodos = new Metodos();
+            Console.WriteLine(misMetodos.Saludo());
+            Console.WriteLine(misMetodos.Sumar(6, 34));
+            Console.WriteLine(misMetodos.Restar(70, 24));
+            Console.WriteLine(misMetodos.Multiplicar(5, 5));
+            Console.WriteLine(misMetodos.Dividir(8, 2));
+            Console.ReadKey();
+        }
+    }
+}
